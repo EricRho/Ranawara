6ffa1179a1b18450fec489d96600d5aea1d89b24
Eric Rho <ericrho@EricMacbookPro.local>
2017-01-03 16:28:58 -0500

    corrections to homepage tests

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   $('.carousel').carousel({full_width: true});
-  // $('.carousel').carousel({dist: 0});
-  // test
-  // 
+  $('header')
+});
+
+$(window).scroll(function() {
+  if (this.document.body.scrollTop < 64) {
+    $('nav').css('background-color', 'grey');
+  } else {
+    $('nav').css('background-color', 'transparent');
+  }
 });
