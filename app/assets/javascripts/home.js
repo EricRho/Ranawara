ae8c59f03b3f0edd4f210633faa97a968c657b3d
Eric Rho <ericrho@EricMacbookPro.fios-router.home>
2017-02-15 15:50:02 -0500

    parallax scroll HOME. test

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+
+	// ------------- VARIABLES ------------- //
+	var ticking = false;
+	var isFirefox = (/Firefox/i.test(navigator.userAgent));
+	var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
+	var scrollSensitivitySetting = 30;
+	var slideDurationSetting = 600;
+	var currentSlideNumber = 0;
+	var totalSlideNumber = $(".background").length;
+
+	// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+	function parallaxScroll(evt) {
+	  if (isFirefox) {
+	    //Set delta for Firefox
+	    delta = evt.detail * (-120);
+	  } else if (isIe) {
+	    //Set delta for IE
+	    delta = -evt.deltaY;
+	  } else {
+	    //Set delta for all other browsers
+	    delta = evt.wheelDelta;
+	  }
+
+	  if (ticking != true) {
+	    if (delta <= -scrollSensitivitySetting) {
+	      //Down scroll
+	      ticking = true;
+	      if (currentSlideNumber !== totalSlideNumber - 1) {
+	        currentSlideNumber++;
+	        nextItem();
+	      }
+	      slideDurationTimeout(slideDurationSetting);
+	    }
+	    if (delta >= scrollSensitivitySetting) {
+	      //Up scroll
+	      ticking = true;
+	      if (currentSlideNumber !== 0) {
+	        currentSlideNumber--;
+	      }
+	      previousItem();
+	      slideDurationTimeout(slideDurationSetting);
+	    }
+	  }
+	}
+
+	// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+	function slideDurationTimeout(slideDuration) {
+	  setTimeout(function() {
+	    ticking = false;
+	  }, slideDuration);
+	}
+
+	// ------------- ADD EVENT LISTENER ------------- //
+	var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+	window.addEventListener('wheel', throttle(parallaxScroll, 60), false);
+	// window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+
+	// ------------- SLIDE MOTION ------------- //
+	function nextItem() {
+	  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
+	  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+	}
+
+	function previousItem() {
+	  var $currentSlide = $(".background").eq(currentSlideNumber);
+	  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+	}
+});
+
+function throttle(fn, threshold, scope) {
+	threshold || (threshold = 60);
+	var last, 
+		deferTimer;
+	return function () {
+		var context = scope || this;
+
+		var now = +new Date,
+			args = arguments;
+		if (last && now < last + threshold) {
+			clearTimeout(deferTimer);
+			deferTimer = setTimeout(function() {
+				last = now;
+				fn.apply(context, args);
+			}, threshold);
+		} else {
+			last = now;
+			fn.apply(context, args);
+		}
+	};
+}
+
--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,26 +1,26 @@
-var ready;
-ready = function() {
-  $('.carousel').carousel({full_width: true});
-  // $('nav').css('background-color', 'transparent');
-  checkTop();
-};
+// var ready;
+// ready = function() {
+//   $('.carousel').carousel({full_width: true});
+//   // $('nav').css('background-color', 'transparent');
+//   checkTop();
+// };
 
-$(document).ready(ready);
-$(document).on('turbolinks:load', ready);
+// $(document).ready(ready);
+// $(document).on('turbolinks:load', ready);
 
-$(window).scroll(function() {
-  if (this.document.body.scrollTop < 64) {
-    // $('nav').css('background-color', 'grey');
-    $('nav').css('background-color', 'transparent');
-  } else {
-    // $('nav').css('background-color', 'transparent');
-    $('nav').css('background-color', 'grey');
-  }
-});
+// $(window).scroll(function() {
+//   if (this.document.body.scrollTop < 64) {
+//     // $('nav').css('background-color', 'grey');
+//     $('nav').css('background-color', 'transparent');
+//   } else {
+//     // $('nav').css('background-color', 'transparent');
+//     $('nav').css('background-color', 'grey');
+//   }
+// });
 
 
-var checkTop = function() {
-  if ($(document.body.scrollTop === 0)) {
-    $('nav').css('background-color', 'transparent');
-  };
-};
+// var checkTop = function() {
+//   if ($(document.body.scrollTop === 0)) {
+//     $('nav').css('background-color', 'transparent');
+//   };
+// };
