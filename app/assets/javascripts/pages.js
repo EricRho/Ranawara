8a60da33a92868bd26e82248bee7b3427c77efb1
Eric Rho <ericrho@EricMacbookPro.fios-router.home>
2017-02-17 11:47:28 -0500

    rework about page title div

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -5,22 +5,10 @@
 //   checkTop();
 // };
 
-// $(document).ready(ready);
-// $(document).on('turbolinks:load', ready);
-
 $(window).scroll(function() {
   if (this.document.body.scrollTop < 64) {
-    // $('nav').css('background-color', 'grey');
     $('nav').css('background-color', 'transparent');
   } else {
-    // $('nav').css('background-color', 'transparent');
     $('nav').css('background-color', 'grey');
   }
 });
-
-
-// var checkTop = function() {
-//   if ($(document.body.scrollTop === 0)) {
-//     $('nav').css('background-color', 'transparent');
-//   };
-// };
