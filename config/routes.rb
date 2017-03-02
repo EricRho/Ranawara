15d919249aaec47c8535f42a899fc74cb2f4b9f6
EricRho <eric.rho.87@gmail.com>
2017-03-02 11:21:31 -0500

    Add remaining Nav pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 
-
-  get 'contact/index'
-
-  # get 'gallery/index'
-
   get 'home/index' => ''
   get '/activities' => 'activities#index', as: 'activities'
   get '/about' => 'about#index', as: 'about'
   get '/gallery' => 'gallery#index', as: 'gallery'
   get '/contact' => 'contact#index', as: 'contact'
+  get '/availability' => 'availability#index', as: 'availiability'
+  get '/FAQ' => 'faq#index', as: 'faq'
   # get 'about/index', to: 'about#index', as: 'about'
 
   root 'home#index'
