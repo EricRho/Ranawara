41f6de627f25a92ece84444d4839fb827cc3b400
EricRho <eric.rho.87@gmail.com>
2017-03-02 14:44:01 -0500

    Create contacts page and include google maps without JS bugs

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,9 +1,7 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
-
 var ready;
+
 ready = function() {
+  console.log('ABOUT');
   var $cont = document.querySelector('.cont');
 
   var $elsArr = [].slice.call(document.querySelectorAll('.el'));
@@ -12,7 +10,6 @@ ready = function() {
   setTimeout(function() {
     $cont.classList.remove('s--inactive');
   }, 200);
-
   $elsArr.forEach(function($el) {
     $el.addEventListener('click', function() {
       console.log($el);
@@ -34,38 +31,3 @@ ready = function() {
 $(document).ready(ready);
 // $(document).on('turbolinks:load', ready);
 // $(document).on('page:change', ready);
-
-
-
-
-// $(document).ready(function() {
-//   console.log('good');
-//   var $cont = document.querySelector('.cont');
-//
-//   var $elsArr = [].slice.call(document.querySelectorAll('.el'));
-//   var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
-//
-//   setTimeout(function() {
-//     $cont.classList.remove('s--inactive');
-//   }, 200);
-//
-//   $elsArr.forEach(function($el) {
-//     console.log('run');
-//     $el.addEventListener('click', function() {
-//       console.log($el);
-//       if (this.classList.contains('s--active')) return;
-//       $cont.classList.add('s--el-active');
-//       this.classList.add('s--active');
-//       console.log('good');
-//     });
-//   });
-//
-//   $closeBtnsArr.forEach(function($btn) {
-//     $btn.addEventListener('click', function(e) {
-//       e.stopPropagation();
-//       $cont.classList.remove('s--el-active');
-//       document.querySelector('.el.s--active').classList.remove('s--active');
-//     });
-//   });
-//
-// });
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
 
+  get 'contact/index'
+
   # get 'gallery/index'
 
   get 'home/index' => ''
   get '/activities' => 'activities#index', as: 'activities'
   get '/about' => 'about#index', as: 'about'
   get '/gallery' => 'gallery#index', as: 'gallery'
+  get '/contact' => 'contact#index', as: 'contact'
   # get 'about/index', to: 'about#index', as: 'about'
 
   root 'home#index'
